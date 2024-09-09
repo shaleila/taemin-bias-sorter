f996bcdc6a6847f75ba247064bcc60228e1d2351
Shaleila <shaleila.louis@gmail.com>
2024-09-09 06:29:58 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,65 @@
+const taeminTracks = [
+    "Move",
+    "Want",
+    "Criminal",
+    "Advice",
+    "Idea",
+    "Press Your Number",
+    "Drip Drop"
+];
+
+let leftIndex = 0;
+let rightIndex = 1;
+let results = [];
+
+function updateOptions() {
+    document.getElementById('options').innerHTML = `
+        <h3>${taeminTracks[leftIndex]} vs ${taeminTracks[rightIndex]}</h3>
+    `;
+}
+
+function sortTracks(winnerIndex, loserIndex) {
+    const winner = taeminTracks[winnerIndex];
+    const loser = taeminTracks[loserIndex];
+
+    if (results.indexOf(winner) === -1) {
+        results.push(winner);
+    }
+    if (results.indexOf(loser) === -1) {
+        results.push(loser);
+    }
+
+    rightIndex++;
+    if (rightIndex >= taeminTracks.length) {
+        leftIndex++;
+        rightIndex = leftIndex + 1;
+    }
+
+    if (leftIndex >= taeminTracks.length - 1) {
+        showResults();
+    } else {
+        updateOptions();
+    }
+}
+
+function showResults() {
+    document.getElementById('container').style.display = 'none';
+    const resultDiv = document.getElementById('result');
+    resultDiv.style.display = 'block';
+
+    const rankings = document.getElementById('rankings');
+    rankings.innerHTML = '';
+    results.forEach(track => {
+        rankings.innerHTML += `<li>${track}</li>`;
+    });
+}
+
+document.getElementById('left-button').addEventListener('click', () => {
+    sortTracks(leftIndex, rightIndex);
+});
+
+document.getElementById('right-button').addEventListener('click', () => {
+    sortTracks(rightIndex, leftIndex);
+});
+
+updateOptions();
